1003bf21606275afde6f4982980a7a172e05e4cb
ilanamost <ilana.mostovski@gmail.com>
2021-02-20 20:20:11 +0200

    change dist path in server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 // serve our static files
-app.use(express.static('./dist/cookaroo'));
+app.use(express.static('./dist/<cookaroo>'));
 
 // wait for a request to any path and redirect all of the requests to index.html
 app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/cookaroo/' }
+    res.sendFile('index.html', { root: 'dist/<cookaroo>/' }
     );
 });
 
