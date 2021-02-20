71574c391db48398f9655f2ca8b149b187d7a748
ilanamost <ilana.mostovski@gmail.com>
2021-02-20 20:09:28 +0200

    1. added server.js file to run the app in production 2. change 'start' script in package.json 3.change 'apiUrl' value in production environment

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// serve our static files
+app.use(express.static('./dist/cookaroo'));
+
+// wait for a request to any path and redirect all of the requests to index.html
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/cookaroo/' }
+    );
+});
+
+// listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080
+app.listen(process.env.PORT || 8080);
