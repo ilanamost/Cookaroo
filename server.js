fef0e4b62c04b60399b3fdced938f7e890f61e4b
ilanamost <ilana.mostovski@gmail.com>
2021-02-21 19:32:24 +0200

    1. change package.json for deployment: add 'dependencies' and 'heroku-postbuild' script 2. change server.js file for deployment: use '_dirname' variable

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// serve our static files
-app.use(express.static('./dist/cookaroo'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/cookaroo'));
 
 // wait for a request to any path and redirect all of the requests to index.html
-app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/cookaroo/' });
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/cookaroo/index.html'));
 });
 
 // listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080
 app.listen(process.env.PORT || 8080);
+
